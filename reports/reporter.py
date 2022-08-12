1a324ca95e7f62d0c4e2d26053a014feac9ff063
Lucas França <lucas.franca@tinnova.com.br>
2022-08-12 14:17:10 -0300

    fix: Remove from DB Models fields that aren't used and fix load_simulation query

--- reports/reporter.py
+++ reports/reporter.py
@@ -1,58 +1,53 @@
-from dataclasses import dataclass
-from datetime import datetime
+import logging
 import os
+from datetime import datetime
 from typing import Any, Dict
-from sqlalchemy import create_engine, func, select, insert
+from uuid import uuid4
+
+from sqlalchemy import create_engine, select, insert
 from sqlalchemy.orm.session import Session
 
 from reports.db_models import IntegrationReport, SimulationSession
-from uuid import uuid4
-
-import logging
 
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
+DATABASE_URL = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
+engine = create_engine(url=DATABASE_URL.format(
+    user=os.getenv('PG_USERNAME'),
+    password=os.getenv('PG_PASSWORD'),
+    host=os.getenv('PG_HOST'),
+    port=os.getenv('PG_PORT'),
+    db=os.getenv('PG_DATABASE'))
+)
 
-@dataclass
+
 class Reporter:
-    session_uuid: str
 
-    def __post_init__(self):
-        db_url = "postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
-        self.engine = create_engine(url=db_url.format(
-            user=os.getenv('PG_USERNAME'),
-            password=os.getenv('PG_PASSWORD'),
-            host=os.getenv('PG_HOST'),
-            port=os.getenv('PG_PORT'),
-            db=os.getenv('PG_DATABASE')))
+    def __init__(self, session_uuid: str):
+        self.session_uuid = session_uuid
+        self.simulation_session = None
         self.__load_simulation()
 
     def __load_simulation(self):
-        stmt = (
-            select(SimulationSession)
-            .join(SimulationSession.simulation)
-            .where(SimulationSession.simulation_uuid == self.session_uuid)
-        )
-
-        with Session(self.engine) as session:
-            self.simulation_session: SimulationSession = session.scalars(stmt).one()
+        statement = select(SimulationSession).where(SimulationSession.simulation_uuid == self.session_uuid)
+        with Session(engine) as session:
+            self.simulation_session: SimulationSession = session.scalars(statement).one()
 
     def save_step_report(self, module: str, function: str, input_data: Dict[str, Any], output_data: Dict[str, Any]):
         stmt = insert(IntegrationReport).values(
             simulation_id=self.simulation_session.simulation_id,
             simulation_uuid=self.simulation_session.simulation_uuid,
+            step_uuid=str(uuid4()),
             data=input_data,
             output=output_data,
-            type="simulation",
             module=module,
             function=function,
-            step_uuid=str(uuid4()),
-            created_at=datetime.now()
-
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
         )
 
-        with self.engine.connect() as connection:
+        with engine.connect() as connection:
             with connection.begin():
                 connection.execute(stmt)
 
@@ -60,17 +55,16 @@ class Reporter:
         stmt = insert(IntegrationReport).values(
             simulation_id=self.simulation_session.simulation_id,
             simulation_uuid=self.simulation_session.simulation_uuid,
+            step_uuid=str(uuid4()),
             data=input_data,
             output={},
             errors=errors,
-            type="simulation",
             module=module,
             function=function,
-            step_uuid=str(uuid4()),
-            created_at=datetime.now()
-
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
         )
 
-        with self.engine.connect() as connection:
+        with engine.connect() as connection:
             with connection.begin():
                 connection.execute(stmt)
